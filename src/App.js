e683d555516115bdea7c00c0a5af40d05e7fb98b
Bekbolot Zhantaev <bekbolot@Bekbolots-MacBook-Pro.local>
2022-12-10 03:08:40 +0600

    first block

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+
+function App() {
+  return (
+     <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+      </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/auth/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+    return(
+        <div>
+            Logingew
+        </div>
+    )
+}
+
+export default Login;
